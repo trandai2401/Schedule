53c7d1b940a6e8bc90c46a13812bee965c806dd9
trandai2401 <trandai2401@gmail.com>
2021-12-23 13:49:07 +0700

    Sửa hiển thị thứ

--- app/src/main/java/com/example/webmasterdotnetvn/schedule/XemChiTietCongViecActivity.java
+++ app/src/main/java/com/example/webmasterdotnetvn/schedule/XemChiTietCongViecActivity.java
@@ -26,22 +26,28 @@ import retrofit2.Response;
 public class XemChiTietCongViecActivity extends AppCompatActivity {
     ArrayList<CongViec> congViecs;
     ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xem_chi_tiet_cong_viec);
-        listView  =findViewById(R.id.listViewXemCongViec);
+        listView = findViewById(R.id.listViewXemCongViec);
         Intent intent = getIntent();
         String idThu = intent.getStringExtra("idThu");
-        ApiService.apiService.getAllCongViecByIdUser(login.staticUser.getId(),idThu).enqueue(
+
+
+        TextView thuTextView = findViewById(R.id.thuTextView);
+        thuTextView.setText("Thứ "+(Integer.parseInt(idThu)+1));
+
+        ApiService.apiService.getAllCongViecByIdUser(login.staticUser.getId(), idThu).enqueue(
                 new Callback<ArrayList<CongViec>>() {
                     @Override
                     public void onResponse(Call<ArrayList<CongViec>> call, Response<ArrayList<CongViec>> response) {
-                        if(response.body()!= null){
+                        if (response.body() != null) {
                             congViecs = response.body();
                             listView.setAdapter(new CongViecAdapter());
                             Toast.makeText(XemChiTietCongViecActivity.this, "Cos du lieu", Toast.LENGTH_SHORT).show();
-                        }else{
+                        } else {
                             Toast.makeText(XemChiTietCongViecActivity.this, "khong co du lieu", Toast.LENGTH_SHORT).show();
 
                         }
@@ -57,8 +63,7 @@ public class XemChiTietCongViecActivity extends AppCompatActivity {
     }
 
 
-
-    class CongViecAdapter extends BaseAdapter{
+    class CongViecAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
@@ -77,32 +82,33 @@ public class XemChiTietCongViecActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View view =getLayoutInflater().inflate(R.layout.xemtkb,null);
-            TextView monHoc =view.findViewById(R.id.monHoc);
+            View view = getLayoutInflater().inflate(R.layout.xemtkb, null);
+            TextView monHoc = view.findViewById(R.id.monHoc);
             TextView gioBatDau = view.findViewById(R.id.gioBatDau);
             TextView GioketThuc = view.findViewById(R.id.GioketThuc);
             ImageView imageView2 = view.findViewById(R.id.imageView2);
-            
+            TextView thu = view.findViewById(R.id.textView2);
+
 
             GioketThuc.setText(congViecs.get(position).getGioKetThuc().toString());
             gioBatDau.setText(congViecs.get(position).getGioBatDau().toString());
 
             monHoc.setText(congViecs.get(position).getNoidung());
-
+            thu.setText("Thứ "+ (Integer.valueOf(congViecs.get(position).getId_thu())+1));
             imageView2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    
+
                     ApiService.apiService.postDeleteCongViec(congViecs.get(position).getId()).enqueue(
                             new Callback<Integer>() {
                                 @Override
                                 public void onResponse(Call<Integer> call, Response<Integer> response) {
-                                    if(response.body() != null){
+                                    if (response.body() != null) {
                                         Integer b = response.body();
                                         congViecs.remove(position);
-notifyDataSetChanged();
+                                        notifyDataSetChanged();
                                         Toast.makeText(XemChiTietCongViecActivity.this, "Đã xóa công việc trong CSDL", Toast.LENGTH_SHORT).show();
-                                    }else{
+                                    } else {
                                         Toast.makeText(XemChiTietCongViecActivity.this, "Có trả về nhưng khum có gì", Toast.LENGTH_SHORT).show();
 
                                     }
@@ -125,12 +131,12 @@ notifyDataSetChanged();
 
                     Intent intent = new Intent(XemChiTietCongViecActivity.this, edit.class);
 
-                    intent.putExtra("id",congViecs.get(position).getId());
-                    intent.putExtra("id_thu",congViecs.get(position).getId_thu());
-                    intent.putExtra("id_user",congViecs.get(position).getId_user());
-                    intent.putExtra("noiDung",congViecs.get(position).getNoidung());
-                    intent.putExtra("startTime",congViecs.get(position).getGioBatDau());
-                    intent.putExtra("endTime",congViecs.get(position).getGioKetThuc());
+                    intent.putExtra("id", congViecs.get(position).getId());
+                    intent.putExtra("id_thu", congViecs.get(position).getId_thu());
+                    intent.putExtra("id_user", congViecs.get(position).getId_user());
+                    intent.putExtra("noiDung", congViecs.get(position).getNoidung());
+                    intent.putExtra("startTime", congViecs.get(position).getGioBatDau());
+                    intent.putExtra("endTime", congViecs.get(position).getGioKetThuc());
                     startActivity(intent);
                 }
             });
